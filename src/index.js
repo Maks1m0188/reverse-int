29a367b940b06f8343124285aca4c9e045606c58
Maks1m0188 <leshiy.maxi@gmail.com>
2022-07-13 14:13:10 +0300

    init: start task

--- src/index.js
+++ src/index.js
@@ -1,3 +1,8 @@
 module.exports = function reverse (n) {
-  
+  const str = Math.abs(n).toString();
+  let res = [];
+  for (let i=str.length-1; i>=0; i--) {
+    res.push(str[i])
+  }
+  return res.join('');
 }
